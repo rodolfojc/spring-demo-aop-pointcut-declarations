38722783dfff21f737ebae996277da41cd7282d9
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-03-01 22:12:23 +0000

    Simple Pointcut declaration
    Demo

--- src/com/aopdemo/aspect/MyDemoLogginAspect.java
+++ src/com/aopdemo/aspect/MyDemoLogginAspect.java
@@ -2,6 +2,7 @@ package com.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,8 +11,13 @@ public class MyDemoLogginAspect {
 	
 	//RELATED ADVICES FOR LOGGING
 	
+	//@POINCUT DECLARATION
+	@Pointcut("execution(* com.aopdemo.dao.*.*(..))")
+	public void forDaoPackageDecl() {}
+	
+	
 	//@BEFORE
-	@Before("execution(* com.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackageDecl()")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("Executing @Before advice on addAccount()");
